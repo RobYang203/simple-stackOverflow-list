d6e08f498212913bd656f19dd3687c8433d8b110
RobYang203 <101m06014@stud.sju.edu.tw>
2021-11-13 22:53:54 +0800

    調整 MainLayout

--- src/layouts/MainLayout/index.js
+++ src/layouts/MainLayout/index.js
@@ -1,16 +1,9 @@
-import React from "react";
-import { Container } from "@material-ui/core";
-import TopBar from "./components/TopBar";
+import React from 'react';
+import { Container } from '@material-ui/core';
+import TopBar from './components/TopBar';
 
-function MainLayout({children}) {
-  return (
-    <Container maxWidth={false} >
-      <TopBar />
-      {children}
-    </Container>
-  );
+function MainLayout({ children }) {
+  return <Container maxWidth={false}>{children}</Container>;
 }
-
-
 
 export default MainLayout;
