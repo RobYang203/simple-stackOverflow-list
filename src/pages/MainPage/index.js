600852af0c44802a98e5a698609e0ca4a0be910c
RobYang203 <101m06014@stud.sju.edu.tw>
2021-11-13 23:45:52 +0800

    建立 SearchBar ui

--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import {
+  AppBar,
+  Button,
+  IconButton,
+  makeStyles,
+  OutlinedInput,
+} from '@material-ui/core';
+import React from 'react';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import SearchIcon from '@material-ui/icons/Search';
+import SearchBar from './components/SearchBar';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    borderRadius: 20,
+  },
+  inputRoot: {
+    borderRadius: 20,
+  },
+  inputAdornedEnd: {
+    borderTopLeftRadius: 20,
+    borderBottomLeftRadius: 20,
+    background: '#fff',
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 function MainPage() {
-    return <div>MainPage</div>
+  const classes = useStyles();
+  return (
+    <div>
+     <SearchBar/>
+    </div>
+  );
 }
-
 
 export default MainPage;
